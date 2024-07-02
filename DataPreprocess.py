5f950ee25f2c8b9fef9d47b4a543ac0aecccf951
Kris <kk07213115@outlook.com>
2024-07-02 17:05:20 +0800

    2024-07-02 17:04: Initial Code Part 1 Commit.

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -1,3 +1,39 @@
+import arcpy
 import os
+import numpy as np
+OriginGlobalMonthlyDNB = r'E:/ShanghaiFactory/Monthcomposition/'
+AllgdbPath = r"E:/ShanghaiFactory/SHMonthlyCompositonGDB/"
+# projectFilepath = r'E:/ShanghaiFactory/sh_Local_Coordinate.prj'
 
-print("Hello World!")
+for year in range(2021, 2022):
+    #open某年的全球月合成
+    path = OriginGlobalMonthlyDNB + str(year)
+    globalmonth = os.listdir(path)
+    #open某年每月的GDB作为数据处理环境
+    gdbpath = 'SH_VIIRS_' + str(year) + '年月合成数据.gdb'
+    arcpy.env.workspace = AllgdbPath + gdbpath
+    arcpy.env.overwriteOutput = True
+    print(arcpy.env.workspace)
+    BJShp = ''
+    for shp in arcpy.ListFeatureClasses('', ''):
+        if 'Shanghaibous' in shp:
+            BJShp = shp
+            print(BJShp)
+    print('............Processing............. ')
+    count = 0
+    for name in globalmonth:
+        if count < 9:
+            #通过文件名过滤原始tif
+            if 'masked' in name:
+                print(name)
+                ras = arcpy.Raster(path + '\\' +name)
+                extractbymask = arcpy.sa.ExtractByMask(ras, BJShp, 'INSIDE')
+                count = count + 1
+                extractbymask.save(arcpy.env.workspace+'\BJ'+str(year)+'0'+str(count))
+        else:
+            if 'masked' in name:
+                print(name)
+                ras = arcpy.Raster(path + '\\' +name)
+                extractbymask = arcpy.sa.ExtractByMask(ras, BJShp, 'INSIDE')
+                count = count + 1
+                extractbymask.save(arcpy.env.workspace+'\BJ'+str(year)+str(count))
